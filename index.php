aa8859d367109c1b5ce9c1f65f6205f34b748b89
Daniel Brewer <robosnake92@gmail.com>
2020-10-17 15:59:24 -0400

    First version of collecting comments. Probably not the most efficient method

--- index.php
+++ index.php
@@ -15,11 +15,39 @@ if ($conn->connect_error) {
 $sql = "SELECT comments FROM sweetwater_test";
 $res = $conn->query($sql);
 
+$candyComments = array();
+$callComments = array();
+$refferedComments = array();
+$signatureComments = array();
+$miscComments = array();
+
 // loop over each comment and print to webpage
 echo "Comments from DB<br>";
 while ($row = $res->fetch_assoc()) {
-    echo "<br>comment = " . $row['comments'];
+    $upperComment = strtoupper($row['comments']);
+    $origComment = $row['comments'];
+    
+    if (strpos($upperComment, "CANDY") !== false) {
+      array_push($candyComments, $origComment);
+    }
+    else if (strpos($upperComment, "CALL") !== false) {
+      array_push($callComments, $origComment);
+    }
+    else if (strpos($upperComment, "REFERR") !== false) {
+      array_push($refferedComments, $origComment);
+    }
+    else if (strpos($upperComment, "SIGNATURE") !== false or strpos($upperComment, " SIGN ") !== false) {
+      array_push($signatureComments, $origComment);
+    }
+    else {
+      array_push($miscComments, $origComment);
+    }
 }
+
+// testing what is actually in the array
+foreach ($miscComments as $miscComment) {
+    echo "comment: $miscComment<br>";
+  }
 
 $conn->close();
 ?> 
